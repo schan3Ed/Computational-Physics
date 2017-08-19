c491211350d7e79ecf526301de7c911110d2082b
Edward Chan <schan3@ncsu.edu>
2017-08-19 17:23:45 -0400

    Finished q1 in hw0
    Still need to finish the report and add examples

---
+++ Hw0/q1.py
@@ -0,0 +1,23 @@
+import scipy as scipy
+import numpy as numpy
+
+def multiply(ma, mb):
+    n = ma.ndim
+    mc = numpy.zeros((n, n))
+    for i in range(0, n):
+        for j in range(0, n):
+            sum = 0
+            for k in range(0, n):
+                mul = 1
+                if (i + j + k) % 2 == 1:
+                    mul = -1
+                sum += ma[i, k] * mb[k, j] * mul
+            mc[i, j] = sum
+    return mc
+
+
+ma = numpy.matrix('1 1; 1 1')
+mb = numpy.matrix('1 1; 1 1')
+print(multiply(ma, mb))
+
+#solved
