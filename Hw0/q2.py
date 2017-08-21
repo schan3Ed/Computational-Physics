21b434fa799d1a0e488b44a99068c8c2e296a222
Mountain Chan <Mountain Chan>
2017-08-20 23:07:40 -0400

    added numpy and cmopleted the q2

--- Hw0/q2.py
+++ Hw0/q2.py
@@ -1,6 +1,6 @@
 from collections import deque
 import copy
-
+import numpy
 
 def rec(head, tail, round_count, all_list, var):
     if(round_count < 1):
@@ -11,7 +11,7 @@ def rec(head, tail, round_count, all_list, var):
         p = list(head)
         p.append(x)
         tail.append(x)
-        print(p, "    ", round_count, "   ", tail, "   ", i)
+#        print(p, "    ", round_count, "   ", tail, "   ", i)
         rec(copy.copy(p), copy.copy(tail), round_count - 1, all_list, var)
 
 
@@ -22,5 +22,13 @@ round_count = 4
 var = 2
 all_list = []
 rec(head, tail, round_count, all_list, var)
-print(all_list)
-        
+print("\nAll possible permutation   ", all_list, "\n")
+
+#Ending the permutation code ehre
+sum = 0
+for n in all_list:
+    ep = n[0] * n[1] + n[1] * n[2] + n[2] * n[3] + n[3] * n[0]
+    sum += numpy.exp(ep * -1) * ep
+
+print(sum)
+
